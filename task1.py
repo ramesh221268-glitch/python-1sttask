c9272ed7974543d0a0237f897e43961d315fcfa4
ramesh221268-glitch <ramesh221268@gmail.com>
2026-03-12 16:02:56 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,83 @@
+# task 1
+print("Hello World","Welcome Python",end="\n")
+print("Laptop","Mouse","Keyboard", sep= " | ")
+# task 2
+name="ravi" 
+age="22"
+city="chennai"
+print(name,age,city, sep= "-")
+
+# task 3
+name,age,student = "meena",20,True
+print(name)
+print(age)
+print(student)
+
+# task 4
+word="python" 
+print(word[0])
+print(word[2])
+print(word[5])
+
+#task 5
+print(25+10)
+print(50-20)
+print(8*5)
+print(100/10)
+print(10%3)
+print(2**4)
+print(20//3)
+
+# task 6
+print(3+2*5**2)
+
+# task 7
+num=50
+num += 25
+print(num)
+
+num=100
+num/=10
+print(num)
+
+# task 8
+print(10>5)
+print(20<15)
+print(5==5)
+print(10!=8)
+print(7>=7)
+print(6<=2)
+
+# task 9
+a = "apple" 
+b = "Apple"
+print(a==b)
+# the statement is false because the a and A value are not same
+
+# task 10
+print(10>5 and 5==5)
+print(5>10 or 10==10)
+print(not(5>2))
+
+# task 11
+numbers =[10,20,30,40,50]
+print(20 in numbers)
+print(60 in numbers)
+print(30 not in numbers)
+
+# task 12
+a=10
+b=20
+temp=a
+a=b
+b=temp
+print(a,b)
+
+# task 13
+a=6
+b=3
+print(a^b)
+# we using 8421
+    #      0110   6
+    #      0011   3
+    #      0101   5
